513e3f5d2effbed4969dde5f1333576c3331f1b9
codeforge2 <codeforge.help@gmail.com>
2024-02-08 18:37:28 +0530

    deleted env

--- CodeForge/TechDip/admin.py
+++ CodeForge/TechDip/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from .models import dev_group
 
 # Register your models here.
+class My_dev_admin(admin.ModelAdmin):
+    list_display = ('name','Tech_stack', 'email', 'Mobile_num')
+
+admin.site.register(dev_group,My_dev_admin)
+
--- CodeForge/TechDip/models.py
+++ CodeForge/TechDip/models.py
@@ -1,3 +1,18 @@
 from django.db import models
 
 # Create your models here.
+class dev_group(models.Model):
+    CHOICES = [
+        ("P", "Python"),
+        ("H", "Html"),
+        ("S", "Sql"),
+        ("D", "Django"),
+        ("Dl", "Data Science Libraries"),
+        ("C", "CSS"),
+        ("J", "Java Script"),
+        ("G", "Git")
+    ]
+    name = models.CharField(max_length = 100)
+    Tech_stack = models.CharField(max_length = 2, choices = CHOICES)
+    email = models.EmailField(max_length = 50, null = False)
+    Mobile_num = models.CharField(max_length = 12)
