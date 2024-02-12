249d0c7366d001b8b8de354769b001b0b81b7081
codeforge2 <codeforge.help@gmail.com>
2024-02-12 17:29:18 +0530

    templates

--- TechDip/views.py
+++ TechDip/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import dev_group
 
 # Create your views here.
 def about_us(request):
-    return render(request, "TechDip/index.html")
+    my_data = dev_group.objects.all()
+    return render(request, "TechDip/AI-html-1.0.0/index.html", {"data": my_data}) 
